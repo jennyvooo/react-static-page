bfddc7dfc63ccd9f1a8914e6eba3def1a02db9e9
Jenny Vo <jennythao117@gmail.com>
2025-02-04 18:10:04 -0500

    Add static page components for Rabbit and the Tortoise

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <div>
+      <p>
+      In this classic fable, the Hare is very proud of his speed and boasts about how he can easily win any race. Tired of his arrogance, the Tortoise challenges the Hare to a race. The confident Hare laughs at the idea of losing to a slow-moving Tortoise but agrees to the race. As the race begins, the Hare takes off quickly, leaving the Tortoise far behind. Confident that he will win, the Hare decides to take a nap halfway through the race.
+      </p>
+      <p>
+      Meanwhile, the Tortoise keeps moving steadily and consistently, never stopping. The Hare sleeps for longer than he intended, and by the time he wakes up, he realizes that the Tortoise has almost reached the finish line. Despite his best efforts to catch up, the Hare is too late. The Tortoise crosses the finish line first, winning the race. The lesson from the story is clear: slow and steady wins the race, emphasizing the value of perseverance and not underestimating others based on appearances.
+      </p>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Moral of the story: "Slow and steady wins the race."
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
